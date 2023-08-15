c1e94076ef6e9116adfd6581c61184914b25ffd7
Josee <josee.wu@vestiairecollective.com>
2023-08-15 17:28:43 +0200

    First commit

---
+++ TestSPMModule/TestSPMModule/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestSPMModule
+//
+//  Created by YiChen WU on 09/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("NOOOOOO!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestSPMModule/TestSPMModule/TestSPMModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSPMModuleApp.swift
+//  TestSPMModule
+//
+//  Created by YiChen WU on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSPMModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
